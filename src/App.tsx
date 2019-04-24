c846149a3fd581c86765e764d158eb3cd1876641
nlnrojas <nln.rojas17@gmail.com>
2019-04-23 22:11:58 -0600

    Linking

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router';
 import './App.scss';
 import WeaponGenerator from './model/WeaponGenerator';
 import WeaponPrototype from './model/WeaponPrototype';
@@ -13,20 +13,30 @@ class App extends Component {
     console.log(WeaponPrototype.getPrototype('Scop-ETA'));
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="App-header">          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">            
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">                
+                <li className="nav-item">
+                  <Link className="nav-link" to="/view/CharacterTable">Ingresar presonaje</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="#">Ver lista de personajes</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <h1>Ingreso de Personajes</h1>
         </header>
+        <body>
+
+        </body>
+        <footer>
+
+        </footer>
       </div>
     );
   }
